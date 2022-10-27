2ce5bf07a3ab8f7949a3924d025766f91c0f97ef
mschenk7474 <masonschenk@gmail.com>
2022-10-26 19:02:09 -0600

    firebase work

---
+++ app/src/main/java/com/example/brighams_bears_flashcards/FirebaseUtils.kt
@@ -0,0 +1,22 @@
+package com.example.brighams_bears_flashcards
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirebaseUtils {
+    private val fireStoreDatabase = FirebaseFirestore.getInstance()
+
+    private fun readData() {
+            FirebaseUtils().fireStoreDatabase.collection("History")
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    querySnapshot.forEach { document ->
+                        Log.d(TAG, "Read document with ID ${document.id}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents $exception")
+                }
+        }
+}
