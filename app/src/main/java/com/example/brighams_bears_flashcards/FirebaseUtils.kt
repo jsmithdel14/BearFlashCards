d2a34f24815dcc7a46b79c85c1af9529892a26d0
mschenk7474 <masonschenk@gmail.com>
2022-10-27 12:42:45 -0600

    firebase work

--- app/src/main/java/com/example/brighams_bears_flashcards/FirebaseUtils.kt
+++ app/src/main/java/com/example/brighams_bears_flashcards/FirebaseUtils.kt
@@ -17,6 +17,7 @@ class FirebaseUtils {
                 }
                 .addOnFailureListener { exception ->
                     Log.w(TAG, "Error getting documents $exception")
+
                 }
         }
 }
