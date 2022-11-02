99178b0dbfa6302f89ff2d31fceac63256c0e7dd
mschenk7474 <masonschenk@gmail.com>
2022-11-02 14:34:41 -0600

    intent maybe done for the first page

--- app/src/main/java/com/example/brighams_bears_flashcards/MainActivity.kt
+++ app/src/main/java/com/example/brighams_bears_flashcards/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.brighams_bears_flashcards
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,23 +11,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonNextHome = findViewById<Button>(R.id.nextbuttonhome)
-        buttonNextHome.setOnClickListener{
-            setContentView(R.layout.main_menu)
-
-            val hiB = findViewById<Button>(R.id.historybutton)
-            val mathB = findViewById<Button>(R.id.mathbutton)
-            val sciB = findViewById<Button>(R.id.sciencebutton)
-            val swB = findViewById<Button>(R.id.sightwordbutton)
-
-            hiB.setOnClickListener{setContentView(R.layout.level_menu)}
-            mathB.setOnClickListener{setContentView(R.layout.level_menu)}
-            sciB.setOnClickListener{setContentView(R.layout.level_menu)}
-            swB.setOnClickListener{setContentView(R.layout.level_menu)}
-
-//            val backB = findViewById<Button>(R.id.backbutton)
-//            backB.setOnClickListener{setContentView(R.layout.activity_main)}
+        buttonNextHome.setOnClickListener {
+            val i = Intent(this, MainMenu::class.java)
+            startActivity(i)
         }
-
-        val test = 2;
+//        buttonNextHome.setOnClickListener{
+//            setContentView(R.layout.main_menu)
+//
+//            val hiB = findViewById<Button>(R.id.historybutton)
+//            val mathB = findViewById<Button>(R.id.mathbutton)
+//            val sciB = findViewById<Button>(R.id.sciencebutton)
+//            val swB = findViewById<Button>(R.id.sightwordbutton)
+//
+//            hiB.setOnClickListener{setContentView(R.layout.level_menu)}
+//            mathB.setOnClickListener{setContentView(R.layout.level_menu)}
+//            sciB.setOnClickListener{setContentView(R.layout.level_menu)}
+//            swB.setOnClickListener{setContentView(R.layout.level_menu)}
+//
+//            val backB = findViewById<Button>(R.id.backbutton)
+//            backB.setOnClickListener{finish()}
+//        }
      }
 }
