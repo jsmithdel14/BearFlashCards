9008fd9d3bd6ef36d5c7cde231cd5577f3ef47e6
mschenk7474 <masonschenk@gmail.com>
2022-11-07 12:25:29 -0700

    all the main buttons done

--- app/src/main/java/com/example/brighams_bears_flashcards/LevelMenu.kt
+++ app/src/main/java/com/example/brighams_bears_flashcards/LevelMenu.kt
@@ -1,5 +1,6 @@
 package com.example.brighams_bears_flashcards
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,22 @@ class LevelMenu : AppCompatActivity() {
         val backButton = findViewById<Button>(R.id.backbutton)
         backButton.setOnClickListener {
             finish()
+        }
+
+        val easyButon = findViewById<Button>(R.id.easybutton)
+        easyButon.setOnClickListener{
+            val i = Intent(this, FlashcardScreen::class.java)
+            startActivity(i)
+        }
+        val medButton = findViewById<Button>(R.id.mediumbutton)
+        medButton.setOnClickListener{
+            val i = Intent(this, FlashcardScreen::class.java)
+            startActivity(i)
+        }
+        val harButton = findViewById<Button>(R.id.hardbutton)
+        harButton.setOnClickListener{
+            val i = Intent(this, FlashcardScreen::class.java)
+            startActivity(i)
         }
     }
 }
