5631cb49cd27d0e4627c444677c333de992ef745
mschenk7474 <masonschenk@gmail.com>
2022-12-05 13:28:07 -0700

    comments and discarded unused files

--- app/src/main/java/com/example/brighams_bears_flashcards/MainActivity.kt
+++ app/src/main/java/com/example/brighams_bears_flashcards/MainActivity.kt
@@ -9,9 +9,12 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // hides action bar
         supportActionBar?.hide()
+        // sets layout
         setContentView(R.layout.activity_main)
 
+        // takes the user to the subject selection menu
         val buttonNextHome = findViewById<Button>(R.id.nextbuttonhome)
         buttonNextHome.setOnClickListener {
             val i = Intent(this, MainMenu::class.java)
--- app/src/main/java/com/example/brighams_bears_flashcards/MainMenu.kt
+++ app/src/main/java/com/example/brighams_bears_flashcards/MainMenu.kt
@@ -8,15 +8,18 @@ import androidx.appcompat.app.AppCompatActivity
 class MainMenu  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // hides action bar
         supportActionBar?.hide()
+        // sets layout
         setContentView(R.layout.main_menu)
 
+        // takes the user back to the previous screen
         val backButton = findViewById<Button>(R.id.backbutton)
         backButton.setOnClickListener {
             finish()
         }
 
-
+        // sets the subject and passes the info to the next screen
         val hisButton = findViewById<Button>(R.id.historybutton)
         hisButton.setOnClickListener{
             val i = Intent(this, LevelMenu::class.java)
@@ -24,6 +27,7 @@ class MainMenu  : AppCompatActivity() {
             i.putExtra("subject", subject)
             startActivity(i)
         }
+        // sets the subject and passes the info to the next screen
         val mathButton = findViewById<Button>(R.id.mathbutton)
         mathButton.setOnClickListener{
             val i = Intent(this, LevelMenu::class.java)
@@ -31,6 +35,7 @@ class MainMenu  : AppCompatActivity() {
             i.putExtra("subject", subject)
             startActivity(i)
         }
+        // sets the subject and passes the info to the next screen
         val sciButton = findViewById<Button>(R.id.sciencebutton)
         sciButton.setOnClickListener{
             val i = Intent(this, LevelMenu::class.java)
